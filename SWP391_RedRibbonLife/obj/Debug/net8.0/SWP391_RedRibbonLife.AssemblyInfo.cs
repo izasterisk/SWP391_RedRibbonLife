81191d58bad9201969d1af48e593e0aa8e9d5c80
izasterisk <nguyenngochoa12112003@gmail.com>
2025-07-16 19:51:03 +0700

    Sua ten doc cho do roi

--- SWP391_RedRibbonLife/obj/Debug/net8.0/SWP391_RedRibbonLife.AssemblyInfo.cs
+++ SWP391_RedRibbonLife/obj/Debug/net8.0/SWP391_RedRibbonLife.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP391_RedRibbonLife")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c47acee4af22426a289a90ba2ad37168a2d641b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8968230734433e5f4a18db26a910138635d30199")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP391_RedRibbonLife")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP391_RedRibbonLife")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
